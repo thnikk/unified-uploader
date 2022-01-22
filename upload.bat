d00759987834194af63c167a2930d0ed574787f9
thnikk <thnikk@users.noreply.github.com>
2022-01-21 20:30:46 -0800

    Updated for 2022 models

--- upload.bat
+++ upload.bat
@@ -2,38 +2,21 @@ echo off
 :menu
 echo This script will help you update the firmware of your keypad.
 echo Please select your model:
-echo  1 - 2K RGBW
-echo  2 - 2K (Only use this one if the colors don't look right)
-echo  3 - 4K RGBW
-echo  4 - 4K (same as 2K)
-echo  5 - 2K Touch
-echo  6 - Macropad
-choice /n /c:123456 /M "Choose an option (1-6) "
+echo  1 - 2K
+echo  2 - 4K
+echo  3 - MegaTouch
+choice /n /c:123 /M "Choose an option (1-3) "
 set SELECTION=%errorlevel%
 
-wmic path Win32_SerialPort | find /I "Serial Device" | tools\awk "{print $13}" > tools\port
+wmic path Win32_SerialPort | find /I "Serial Device" | tools\win\awk "{print $13}" > tools\port
 set /p PORT=< tools\port
 mode %PORT%:1200,n,8,1 > nul
 echo Resetting the board...
 timeout 3 > nul
-wmic path Win32_SerialPort | find /I "Serial Device" | tools\awk "{print $13}" > tools\port
+wmic path Win32_SerialPort | find /I "Serial Device" | tools\win\awk "{print $13}" > tools\port
 set /p PORT=< tools\port
 
-IF "%SELECTION%"=="1" (
-set MODEL=2kW
-) ELSE IF "%SELECTION%"=="2" (
-set MODEL=2k
-) ELSE IF "%SELECTION%"=="3" (
-set MODEL=4kW
-) ELSE IF "%SELECTION%"=="4" (
-set MODEL=4k
-) ELSE IF "%SELECTION%"=="5" (
-set MODEL=2kTouch
-) ELSE IF "%SELECTION%"=="6" (
-set MODEL=Macro
-)
-
-echo Uploading code for %MODEL% Keypad...
-tools\bossac.exe -i --port=%PORT% -U true -i -e -w -v firmware/%MODEL%.bin -R > nul
+echo Uploading code for %MODEL% keypad...
+tools\win\bossac.exe -i --port=%PORT% -U true -i -e -w -v firmware/%MODEL%.bin -R > nul
 echo Done! Press any key to close.
 pause > nul
--- upload.sh
+++ upload.sh
@@ -10,21 +10,15 @@ if [ -z "$1" ]; then
     echo "This script will help you update the firmware of your keypad."
     echo "Please select your model:"
     echo "1 - 2k"
-    echo "2 - 2k RGBW (select this if the colors don't look right with the 2k option"
-    echo "3 - 4k"
-    echo "4 - 4K RGBW (same as 2k RGBW)"
-    echo "5 - 2k Touch"
-    echo "6 - Macropad"
-    echo "Choose an option (1-6)"
+    echo "2 - 4k"
+    echo "3 - MegaTouch"
+    echo "Choose an option (1-3)"
     read -r SELECTION
 
     case $SELECTION in
         1) MODEL="2k" ;;
-        2) MODEL="2kW" ;;
-        3) MODEL="4k" ;;
-        4) MODEL="4kW" ;;
-        5) MODEL="2kTouch" ;;
-        6) MODEL="Macro" ;;
+        2) MODEL="4k" ;;
+        3) MODEL="MegaTouch" ;;
     esac
 else
     MODEL=$1
@@ -33,8 +27,8 @@ fi
 echo "Resetting the board..."
 stty -F "$PORT" 1200
 sleep 2
-echo "Uploading code for $MODEL"
+echo "Uploading code for $MODEL."
 echo "File should be at firmware/$MODEL.bin"
-tools/bossac -d -p "$PORT" -e -w -v --reset --offset 0x2000 "firmware/$MODEL.bin"
+tools/linux-x86/bossac -d -p "$PORT" -e -w -v --reset --offset 0x2000 "firmware/$MODEL.bin"
 echo "Done! Press enter to exit..."
 [ -z $1 ] && read -r
